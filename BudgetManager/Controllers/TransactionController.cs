b1cb3ff75d3f1758023e182697e9041fb6c03476
Jakub <jakub26800@gmail.com>
2024-08-27 13:45:16 +0200

    Added controller implmentation

---
+++ BudgetManager/Controllers/TransactionController.cs
@@ -0,0 +1,119 @@
+﻿using BudgetManager.Dto;
+using BudgetManager.Exceptions;
+using BudgetManager.Exceptions.TransactionExceptions;
+using BudgetManager.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetManager.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var transaction = _transactionService.RetrieveTransaction(id);
+                return Ok(transaction);
+            }
+
+            catch (Exception e)
+            {
+                return Conflict(e.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var transactions = _transactionService.RetrieveTransactions();
+                return Ok(transactions);
+            }
+            catch(Exception e)
+            {
+                return Conflict(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(AddTransactionDto dto)
+        {
+            try
+            {
+                var transaction = _transactionService.AddTransaction(dto);
+                return Created($"api/transactions/{transaction.Id}", transaction);
+            }
+            catch(NullPointerException e)
+            {
+                return Conflict(e.Message);
+            }            
+            catch(BadStringLengthException e)
+            {
+                return Conflict(e.Message);
+            }            
+            catch(BadValueException e)
+            {
+                return Conflict(e.Message);
+            }
+            catch (Exception e)
+            {
+                return Conflict(e.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateTransactionDto dto)
+        {
+            try
+            {
+                _transactionService.UpdateTransaction(dto);
+                return NoContent();
+            }
+            catch (NullPointerException e)
+            {
+                return Conflict(e.Message);
+            }
+            catch (BadStringLengthException e)
+            {
+                return Conflict(e.Message);
+            }
+            catch (BadValueException e)
+            {
+                return Conflict(e.Message);
+            }
+            catch (Exception e)
+            {
+                return Conflict(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _transactionService.DeleteTransaction(id);
+                return NoContent();
+            }
+            catch(TransactionNotFoundException e)
+            {
+                return Conflict(e.Message);
+            }
+            catch(Exception e)
+            {
+                return Conflict(e.Message);
+            }
+        }
+    }
+}
