a30edfba404d14138b844c9db88ff0831d44916f
Jakub <jakub26800@gmail.com>
2024-09-12 21:50:21 +0200

    Added MonthName extension method. Fixed site styling.

---
+++ UI/Extensions/MonthNameExtension.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace UI.Extensions
+{
+    public static class MonthNameExtension
+    {
+        public static string GetMonthName(this DateTime date) => 
+            date.ToString("MMMM", CultureInfo.CreateSpecificCulture("en"));
+    }
+}
