b06464d62f22f45716b2335333ec9ad328544ae3
Jakub <jakub26800@gmail.com>
2024-09-13 15:42:01 +0200

    Removed IncomeType property from model and cmponents.

--- UI/Models/TransactionViewModel.cs
+++ UI/Models/TransactionViewModel.cs
@@ -3,7 +3,6 @@
     public class TransactionViewModel
     {
         public int Id { get; set; }
-        public bool IncomeType { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
         public DateTime? Date { get; set; }
