677a098c2b43a877f76f93084f93e36ec852fee0
Jakub <jakub26800@gmail.com>
2024-09-21 22:28:25 +0200

    Added add pattern tab.

---
+++ UI/Pages/MyPages/OptionsPage/Pattern.razor.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using UI.Models;
+
+namespace UI.Pages.MyPages.OptionsPage
+{
+    public partial class Pattern
+    {
+        [Inject] public HttpClient httpClient { get; set; }
+        [Inject]public ISnackbar snackbar { get; set; }
+        private PatternViewModel model = new PatternViewModel();
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+        private async Task AddPattern()
+        {
+            var request = await httpClient.PostAsJsonAsync<PatternViewModel>("/api/pattern", model);
+            if (request.StatusCode == System.Net.HttpStatusCode.Created)
+                snackbar.Add("Successfully added new pattern.", Severity.Success);
+            else
+                snackbar.Add("Something went wrong", Severity.Error);
+        }
+    }
+}
