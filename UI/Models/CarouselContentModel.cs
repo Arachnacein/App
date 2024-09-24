a9352989d13c857d75774c1424bbc753182baa07
Jakub <jakub26800@gmail.com>
2024-09-24 11:13:26 +0200

    Added carusel content with random images.

---
+++ UI/Models/CarouselContentModel.cs
@@ -0,0 +1,13 @@
+﻿namespace UI.Models
+{
+    public class CarouselContentModel
+    {
+        public string ImageUrl { get; set; }
+        public string Header { get; set; }
+        public string Description { get; set; }
+
+        public bool IsActionContent { get; set; }
+        public string? ActionText { get; set; }
+        public string? ActionUrl { get; set; }
+    }
+}
