cdbf80aa06593401bb959f5104f2f9304e8acb26
Jakub <jakub26800@gmail.com>
2024-09-29 15:35:17 +0200

    Added AddIncomeDialog validation. Added ValidatorClass to ViewModelClass.

--- UI/Models/IncomeViewModel.cs
+++ UI/Models/IncomeViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace UI.Models
+﻿using FluentValidation;
+
+namespace UI.Models
 {
     public class IncomeViewModel
     {
@@ -6,5 +8,21 @@
         public string Name { get; set; }
         public double Amount { get; set; }
         public DateTime? Date { get; set; }
+    }
+
+    internal class IncomeViewModelValidation : AbstractValidator<IncomeViewModel>
+    {
+        public IncomeViewModelValidation() 
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name field can't be empty.")
+                .MinimumLength(5).WithMessage("Name should be at least 5 characters long.")
+                .MaximumLength(50).WithMessage("Name should be less than 50 characters.");
+
+            RuleFor(x => x.Amount)
+                .NotEmpty().WithMessage("Price field can't be empty.")
+                .GreaterThanOrEqualTo(0d).WithMessage("Amount na't be lower than 0.")
+                .Must(amount => !double.IsNaN(amount) && !double.IsInfinity(amount)).WithMessage("Amount must be a valid number.");
+        }
     }
 }
