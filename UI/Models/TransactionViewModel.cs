dea07679deb1b092c5ef3a83a8dec0cb3ba15f82
Jakub <jakub26800@gmail.com>
2024-09-29 15:08:56 +0200

    Added AddTransactionDialog validation. Added ValidatorClass to ViewModelClass. Added FluentValidation extension method.

--- UI/Models/TransactionViewModel.cs
+++ UI/Models/TransactionViewModel.cs
@@ -1,4 +1,6 @@
-﻿namespace UI.Models
+﻿using FluentValidation;
+
+namespace UI.Models
 {
     public class TransactionViewModel
     {
@@ -8,5 +10,25 @@
         public DateTime? Date { get; set; }
         public double Price { get; set; }
         public TransactionCategoryEnum Category { get; set; }
+    }
+    internal class TransactionViewModelValidator : AbstractValidator<TransactionViewModel>
+    {
+        public TransactionViewModelValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name field can't be empty.")
+                .MinimumLength(5).WithMessage("Name should be at least 5 characters long.")
+                .MaximumLength(15).WithMessage("Name should be less than 15 characters.");
+
+            RuleFor(x => x.Description)
+                .MinimumLength(5).WithMessage("Description should be at least 5 characters long.")
+                .MaximumLength(50).WithMessage("Description should be less than 50 characters.");
+
+            RuleFor(x => x.Price)
+                .NotEmpty().WithMessage("Price field can't be empty.")
+                .GreaterThanOrEqualTo(0d).WithMessage("Price na't be lower than 0.")
+                .Must(price => !double.IsNaN(price) && !double.IsInfinity(price)).WithMessage("Price must be a valid number.");
+
+        }
     }
 }
