b317d0bf41d2404d92a072797d6d9d74b5782346
Jakub <jakub26800@gmail.com>
2024-10-02 15:40:26 +0200

    Added EditInomeDialog.

---
+++ UI/Components/Dialogs/EditIncomeDialog.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using UI.Models;
+
+namespace UI.Components.Dialogs
+{
+    public partial class EditIncomeDialog
+    {
+        [CascadingParameter] MudDialogInstance MudDialogInstance { get; set; }
+        [Parameter] public IncomeViewModel model { get; set; }
+        [Parameter] public Func<Task> Refresh {  get; set; }
+        [Inject] private HttpClient httpClient {  get; set; }
+        [Inject] private ISnackbar snackbar { get; set; }
+        private IncomeViewModel Model = new IncomeViewModel();
+        private IncomeViewModelValidation IncomeValidator { get; } = new IncomeViewModelValidation();
+        private MudForm Form;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Model = model;
+        }
+        private async Task Submit()
+        {
+            await Form.Validate();
+            if (!Form.IsValid)
+                return;
+
+            var request = await httpClient.PutAsJsonAsync<IncomeViewModel>("/api/income",Model);
+            if (request.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                snackbar.Add("Income updated successfully", Severity.Success);
+                MudDialogInstance.Cancel();
+                if(Refresh != null) 
+                    await Refresh.Invoke();
+            }
+            else
+                snackbar.Add("Something went wrong", Severity.Error);
+        }
+        private async Task Cancel() => MudDialogInstance.Cancel();
+    }
+}
