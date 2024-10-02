e488940beb34d0ac671692f53ff1607035a110e7
Jakub <jakub26800@gmail.com>
2024-10-02 13:30:58 +0200

    Added Incomes option page. Excended options navigation.

---
+++ UI/Pages/MyPages/OptionsPage/Income.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using UI.Models;
+
+namespace UI.Pages.MyPages.OptionsPage
+{
+    public partial class Income
+    {
+        [Inject] HttpClient httpClient {  get; set; }
+        private List<IncomeViewModel> incomes = new List<IncomeViewModel>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadIncomes();
+        }
+
+        private async Task LoadIncomes()
+        {
+            incomes = await httpClient.GetFromJsonAsync<List<IncomeViewModel>>("/api/income");
+        }
+        private async Task EditIncome(IncomeViewModel model)
+        {
+
+        }
+    }
+}
