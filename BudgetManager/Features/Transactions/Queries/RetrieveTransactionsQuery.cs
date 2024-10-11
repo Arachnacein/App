a64d3a938b98581ee1c9b578f9c20a50cda68ab3
Jakub <jakub26800@gmail.com>
2024-10-11 18:46:55 +0200

    Added RetrieveTransactionsQuery and Handler. Fixed GetAll Controller.

--- BudgetManager/Features/Transactions/Queries/RetrieveTransactionsQuery.cs
+++ BudgetManager/Features/Transactions/Queries/RetrieveTransactionsQuery.cs
@@ -1,10 +1,38 @@
-﻿using BudgetManager.Dto.Transaction;
+﻿using BudgetManager.Data;
+using BudgetManager.Dto.Transaction;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace BudgetManager.Features.Transactions.Queries
 {
-    public class RetrieveTransactionsQuery : IRequest<List<TransactionDto>>
-    {
+    public record RetrieveTransactionsQuery : IRequest<IEnumerable<TransactionDto>>
+    { }
 
+    public class RetrieveTransactionsQueryhandler : IRequestHandler<RetrieveTransactionsQuery, IEnumerable<TransactionDto>>
+    {
+        private readonly BudgetDbContext _dbContext;
+
+        public RetrieveTransactionsQueryhandler(BudgetDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<TransactionDto>> Handle(RetrieveTransactionsQuery request, CancellationToken cancellationToken)
+        {
+            var query = "SELECT * FROM BudgetDB.dbo.Transactions";
+            var response = await _dbContext.Transactions
+                                .FromSqlRaw(query)
+                                .Select(transaction => new TransactionDto
+                                {
+                                    Id = transaction.Id,
+                                    Name = transaction.Name,
+                                    Description = transaction.Description,
+                                    Date = transaction.Date,
+                                    Price = transaction.Price,
+                                    Category = transaction.Category
+                                })
+                                .ToListAsync(cancellationToken);
+            return response;
+        }
     }
 }
