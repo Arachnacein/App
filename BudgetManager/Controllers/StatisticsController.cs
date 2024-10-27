9e2ff65536e7eb1ba1ae4165bcd41fa174a1ceca
Jakub <jakub26800@gmail.com>
2024-10-27 21:58:54 +0100

    Added GetTotalExpenses statistic (Query, handler, controller endpoint, GUI and ocelot config).

---
+++ BudgetManager/Controllers/StatisticsController.cs
@@ -0,0 +1,26 @@
+﻿using BudgetManager.Features.Statistics.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StatisticsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetTotalExpenses")]
+        public async Task<IActionResult> GetTotalExpenses()
+        {
+            var query = new RetrieveTotalExpensesQuery();
+            var response = await _mediator.Send(query);
+            return Ok(response);
+        }
+    }
+}
---
+++ UI/Components/StatisticsComponent.razor.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace UI.Components
+{
+    public partial class StatisticsComponent
+    {
+        [Inject] HttpClient httpClient { get; set; }
+        private double TotalExpensesProp { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetTotalExpenses();
+        }
+        private async Task GetTotalExpenses()
+        {
+            TotalExpensesProp = await httpClient.GetFromJsonAsync<double>("/api/statistics/GetTotalExpenses");
+            StateHasChanged();
+        }
+    }
+}
