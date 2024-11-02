ba2a9b8e44c879eb844e5932fccfc3b0c02e13af
Jakub <jakub26800@gmail.com>
2024-11-02 20:46:11 +0100

    Added filter method and counter.

--- UI/Pages/MyPages/OptionsPage/Income.razor.cs
+++ UI/Pages/MyPages/OptionsPage/Income.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MudBlazor;
 using UI.Components.Dialogs;
+using UI.Extensions;
 using UI.Models.ViewModels;
 
 namespace UI.Pages.MyPages.OptionsPage
@@ -10,15 +11,32 @@ namespace UI.Pages.MyPages.OptionsPage
         [Inject] HttpClient httpClient {  get; set; }
         [Inject] IDialogService dialogService { get; set; }
         private List<IncomeViewModel> incomes = new List<IncomeViewModel>();
+        private List<IncomeViewModel> filteredIncomes = new List<IncomeViewModel>();
+        private string _searchPhrase = string.Empty;
+        public string searchPhrase 
+        {
+            get => _searchPhrase;
+            set                     //for dynamic filtering
+            {
+                if (_searchPhrase != value)
+                {
+                    _searchPhrase = value;
+                    FilterIncomes();
+                }
+            }
+        }
+        private int IncomesCounter { get; set; } = 0;
 
         protected override async Task OnInitializedAsync()
         {
             await LoadIncomes();
+            IncomesCounter = incomes.Count();
         }
 
         private async Task LoadIncomes()
         {
             incomes = await httpClient.GetFromJsonAsync<List<IncomeViewModel>>("/api/income");
+            filteredIncomes = incomes;
             StateHasChanged();
         }
         private async Task EditIncome(IncomeViewModel model)
@@ -29,6 +47,21 @@ namespace UI.Pages.MyPages.OptionsPage
             parameters["Refresh"] = new Func<Task>(LoadIncomes);
 
             await dialogService.ShowAsync<EditIncomeDialog>("Edit", parameters, options);
+        }
+        private void FilterIncomes()
+        {
+            if (string.IsNullOrWhiteSpace(searchPhrase) || searchPhrase.Length < 3)
+                filteredIncomes = incomes;
+            else
+            {
+                filteredIncomes = incomes.Where(x =>
+                    (x.Name?.Contains(searchPhrase, StringComparison.OrdinalIgnoreCase) ?? false) ||
+                    (x.Amount.ToString().Contains(searchPhrase, StringComparison.OrdinalIgnoreCase)) ||
+                    (x.Date.ShortFormat().Contains(searchPhrase, StringComparison.OrdinalIgnoreCase))
+                ).ToList();
+            }
+            IncomesCounter = filteredIncomes.Count();
+            StateHasChanged();
         }
     }
 }
