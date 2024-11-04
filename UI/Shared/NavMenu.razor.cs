4710e3c4bb5b9acc7303f127ba4e4c55a3817162
Jakub <jakub26800@gmail.com>
2024-11-04 17:48:37 +0100

    Added  .resx files for PL and EN. Added js script in _Host.cshtml. Added nuget Localization extension. Added switching language logic in NavMenu.razor.cs. Changed usual texts to @Localizers.

---
+++ UI/Shared/NavMenu.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace UI.Shared
+{
+    public partial class NavMenu
+    {
+        [Inject] NavigationManager NavManager { get; set; }
+        [Inject] IJSRuntime JSRuntime { get; set; }
+
+        private async void SetPolish() => await SetCulture("pl-PL");
+        private async void SetEnglish() => await SetCulture("en-UK");
+
+        private async Task SetCulture(string culture)
+        {
+            var uri = $"{NavManager.Uri}?culture={culture}";
+            await JSRuntime.InvokeVoidAsync("blazorCulture.set", culture);
+            NavManager.NavigateTo(uri, forceLoad: true);
+        }
+    }
+}
