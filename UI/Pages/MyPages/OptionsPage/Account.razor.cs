87c49d6801ff15bfc4f5c939a0364af74c0827de
Jakub <jakub26800@gmail.com>
2024-11-09 19:48:07 +0100

    Added top navigation Icon with menu. Added account page. Added localization.

---
+++ UI/Pages/MyPages/OptionsPage/Account.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace UI.Pages.MyPages.OptionsPage
+{
+    public partial class Account
+    {
+        [Inject] private IStringLocalizer<Account> Localizer {  get; set; }
+
+    }
+}
