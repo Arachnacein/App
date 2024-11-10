747659190619de5ff3445f9ce43eed0fecf3fe15
Jakub <jakub26800@gmail.com>
2024-11-10 17:42:12 +0100

    Added IdentityManager and keycloak configuration to docker-compose. Addded jwt configuration to apigateway -Program.cs.

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,15 +1,29 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot_config.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "http://keycloak:8080/realms/IdentityRealm"; 
+        options.Audience = "apigateway"; 
+        options.RequireHttpsMetadata = false;
+    });
+
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 await app.UseOcelot();
 
 app.Run();
