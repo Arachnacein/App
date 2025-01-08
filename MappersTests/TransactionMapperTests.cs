19b4ecc2d1b3b0641b2a1ee13194d17e150ff50a
Jakub <jakub26800@gmail.com>
2025-01-08 13:59:05 +0100

    Added MapperTests project. Added test cases for TransactonMapper.

---
+++ MappersTests/TransactionMapperTests.cs
@@ -0,0 +1,47 @@
+﻿
+namespace MappersTests
+{
+    public class TransactionMapperTests
+    {
+        [Fact]
+        public async Task  Map_TransactionToTransactionDto_ShouldReturnTransationDto_WhenCalled()
+        {
+
+        }       
+        [Fact]
+        public async Task  Map_SaveTransactionCommandToAddTransactionDto_ShouldReturnAddTransationDto_WhenCalled()
+        {
+
+        }       
+        [Fact]
+        public async Task  Map_UpdateTransactionCommandToUpdateTransactionDto_ShouldReturnUpdateTransationDto_WhenCalled()
+        {
+
+        }       
+        [Fact]
+        public async Task  Map_TransactionDtoToTransaction_ShouldReturnTransation_WhenCalled()
+        {
+
+        }        
+        [Fact]
+        public async Task  Map_AddTransactionDtoToTransaction_ShouldReturnAddTransation_WhenCalled()
+        {
+
+        }        
+        [Fact]
+        public async Task  Map_UpdateTransactionDtoToTransaction_ShouldReturnUpdateTransation_WhenCalled()
+        {
+
+        }        
+        [Fact]
+        public async Task  MapElements_TransactionListToTransactionDtoList_ShouldReturnTransationDtoList_WhenCalled()
+        {
+
+        }        
+        [Fact]
+        public async Task  MapElements_TransactionDtoListToTransactionList_ShouldReturnTransationList_WhenCalled()
+        {
+
+        } 
+    }
+}
